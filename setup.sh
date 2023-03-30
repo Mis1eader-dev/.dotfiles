796db0a42b2efe04d4e833130cee82f28ceffda3
Mis1eader-dev <muhammad.luqman.faiq@gmail.com>
2023-03-30 17:38:40 +0300

    FInalized the setup

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt autoremove -y
 
+
+
 # OpenSSL
 sudo apt install -y libssl-dev
 
@@ -16,18 +18,38 @@ sudo apt install -y git
 sudo add-apt-repository -y universe
 sudo apt install -y libfuse2
 
+
+
+# Tmux
+sudo apt install -y tmux
+
+# Tmux theme
+git clone https://github.com/jimeh/tmux-themepack.git ~/.config/tmux/plugins/tmux-themepack
+
+
+
 # Neovim
 sudo wget -P /usr/bin/ https://github.com/neovim/neovim/releases/latest/download/nvim.appimage &&\
 sudo chmod +x /usr/bin/nvim.appimage &&\
 sudo mv /usr/bin/nvim.appimage /usr/bin/nvim
 
+
+
 # g++
 sudo apt install -y g++
+
+# CMake
+sudo apt install -y cmake
+
+# Ninja
+sudo apt install -y ninja-build
+
+
 
 # Clangd for Neovim
 sudo apt install -y clangd-15
 
-# Node.js
+# Node.js for Coc
 curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
 sudo apt install -y nodejs
 
