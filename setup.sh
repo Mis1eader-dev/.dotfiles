89ddbae1ef40494f87ff6912d3e4a88fd4ecca6f
Mis1eader-dev <muhammad.luqman.faiq@gmail.com>
2023-03-30 13:28:46 +0300

    Added safety for the setup

--- setup.sh
+++ setup.sh
@@ -17,8 +17,8 @@ sudo add-apt-repository -y universe
 sudo apt install -y libfuse2
 
 # Neovim
-sudo wget -P /usr/bin/ https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-sudo chmod +x /usr/bin/nvim.appimage
+sudo wget -P /usr/bin/ https://github.com/neovim/neovim/releases/latest/download/nvim.appimage &&\
+sudo chmod +x /usr/bin/nvim.appimage &&\
 sudo mv /usr/bin/nvim.appimage /usr/bin/nvim
 
 # g++
