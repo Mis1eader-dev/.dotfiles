42886999c2506ba3b558198dff3ff6512b9fb5a2
Mis1eader-dev <muhammad.luqman.faiq@gmail.com>
2023-04-01 17:58:42 +0300

    Fixed indentation consistency of `ask`

--- setup.sh
+++ setup.sh
@@ -2,39 +2,39 @@
 
 # Prompt for Yes/No (Ref: https://gist.github.com/karancode/f43bc93f9e47f53e71fa29eed638243c)
 ask() {
-    local prompt default reply
+	local prompt default reply
 
-    if [[ ${2:-} = 'Y' ]]; then
-        prompt='Y/n'
-        default='Y'
-    elif [[ ${2:-} = 'N' ]]; then
-        prompt='y/N'
-        default='N'
-    else
-        prompt='y/n'
-        default=''
-    fi
+	if [[ ${2:-} = 'Y' ]]; then
+		prompt='Y/n'
+		default='Y'
+	elif [[ ${2:-} = 'N' ]]; then
+		prompt='y/N'
+		default='N'
+	else
+		prompt='y/n'
+		default=''
+	fi
 
-    while true; do
+	while true; do
 
-        # Ask the question (not using "read -p" as it uses stderr not stdout)
-        echo -n "$1 [$prompt] "
+		# Ask the question (not using "read -p" as it uses stderr not stdout)
+		echo -n "$1 [$prompt] "
 
-        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
-        read -r reply </dev/tty
+		# Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+		read -r reply </dev/tty
 
-        # Default?
-        if [[ -z $reply ]]; then
-            reply=$default
-        fi
+		# Default?
+		if [[ -z $reply ]]; then
+			reply=$default
+		fi
 
-        # Check if the reply is valid
-        case "$reply" in
-            Y*|y*) return 0 ;;
-            N*|n*) return 1 ;;
-        esac
+		# Check if the reply is valid
+		case "$reply" in
+			Y*|y*) return 0 ;;
+			N*|n*) return 1 ;;
+		esac
 
-    done
+	done
 }
 
 
