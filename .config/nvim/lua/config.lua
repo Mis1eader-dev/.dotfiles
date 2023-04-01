db3ab82efe0dac8c15f55c0913f5cd85a4cd5185
Mis1eader-dev <muhammad.luqman.faiq@gmail.com>
2023-04-01 10:09:53 +0300

    Changed a few configuration options

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -1,8 +1,17 @@
-vim.wo.fillchars='eob: '
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.wo.number = true
+vim.opt.smartindent = true
+vim.wo.number = true -- Show line numbers
+vim.opt.list = true
+vim.opt.listchars = { trail = '·', tab = '| ', leadmultispace = '|···', precedes = '<', extends = '>' }
 vim.opt.cursorline = true
+vim.wo.fillchars='eob: '
+vim.opt.wrap = false
+vim.opt.sidescroll = 1
+--vim.opt.sidescrolloff = 5
 --vim.opt.laststatus = 0 -- To disable status line
 vim.opt.termguicolors = true
+vim.opt.showmode = false
+vim.opt.history = 1000
+vim.opt.completeopt = { "menuone", "longest", "preview", "noselect" }
 vim.inccommand=nosplit
